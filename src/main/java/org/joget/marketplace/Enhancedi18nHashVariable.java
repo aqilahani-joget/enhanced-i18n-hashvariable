c25ab2d1a060ab8510191faa65b3e0159ceed0c4
aqilahani-joget <nor.aqilahani@joget.com>
2025-07-07 23:46:28 +0800

    Enhanced i18n Hash Variable modification to Appp Messages Hash Variable

---
+++ src/main/java/org/joget/marketplace/Enhancedi18nHashVariable.java
@@ -0,0 +1,42 @@
+package org.joget.marketplace;
+
+import org.joget.apps.app.lib.AppMessageHashVariable;
+import org.joget.apps.app.service.AppUtil;
+import org.joget.commons.util.LogUtil;
+import org.joget.commons.util.ResourceBundleUtil;
+
+public class Enhancedi18nHashVariable extends AppMessageHashVariable {
+
+    @Override
+    public String processHashVariable(String variableKey) {
+        
+        String label = AppUtil.replaceAppMessage(variableKey);
+        if (label != null && !label.equals(variableKey)) {
+            return label;
+        }
+        String test = ResourceBundleUtil.getMessage(variableKey);
+        LogUtil.info(getClass().getName(), test);
+        return test;
+    }
+
+    @Override
+    public String getName() {
+        return "Enhanced i18n Hash Variable";
+    }
+
+    @Override
+    public String getVersion() {
+        return "8.0.1";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Returns Platform Translation for missing App Messages";
+    }
+
+    @Override
+    public String getLabel() {
+        return "Enhanced i18n Hash Variable";
+    }
+    
+}
